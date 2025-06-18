05a752e846b304f14c31809d95d33d44d6938650
Richard PC <rifernun@gmail.com>
2025-06-18 08:45:34 -0300

    Chegando ao RestFul

--- API/index.js
+++ API/index.js
@@ -73,18 +73,69 @@ var DB = { //db falso
 //endpoint
 app.get('/games',auth,(req,res)=>{
     
+    var HATEOAS = [
+        {
+            href: 'http://localhost/8080/game/0',
+            method: 'GET',
+            rel: 'get_game'
+        },
+        {
+            href: 'http://localhost/8080/game/0',
+            method: 'DELETE',
+            rel: 'delete_game'
+        },
+        {
+            href: 'http://localhost/8080/game/0',
+            method: 'PUT',
+            rel: 'update_game'
+        },
+        {
+            href: 'http://localhost/8080/game',
+            method: 'POST',
+            rel: 'create_game'
+        },
+        {
+            href: 'http://localhost/8080/auth',
+            method: 'POST',
+            rel: 'login'
+        }
+    ];
+
     res.statusCode = 200; //requisição feita com sucesso
-    res.json(DB.games); //listagem de games
+    res.json({games: DB.games, _links: HATEOAS}); //listagem de games
 });
 app.get('/game/:id',auth, (req,res)=>{
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
         var id = parseInt(req.params.id);
+
+        var HATEOAS = [
+            {
+                href: 'http://localhost/8080/game/'+id,
+                method: 'GET',
+                rel: 'get_game'
+            },
+            {
+                href: 'http://localhost/8080/game/'+id,
+                method: 'DELETE',
+                rel: 'delete_game'
+            },
+            {
+                href: 'http://localhost/8080/game/'+id,
+                method: 'PUT',
+                rel: 'edit_game'
+            },
+            {
+                href: 'http://localhost/8080/games',
+                method: 'GET',
+                rel: 'get_all_games'
+            }
+        ];
         var game = DB.games.find(g => g.id == id); //findOne
         if(game != undefined){
             res.statusCode = 200;
-            res.json(game);
+            res.json({game: game, _links: HATEOAS});
         }else{
             res.sendStatus(404);
         }
@@ -119,6 +170,18 @@ app.put('/game/:id',auth, (req,res) => {
         res.sendStatus(400)
     }else{
         var id = parseInt(req.params.id);
+        var HATEOAS = [
+            {
+                href: 'http://localhost/8080/game/'+id,
+                method: 'GET',
+                rel: 'get_game'
+            },
+            {
+                href: 'http://localhost/8080/game/'+id,
+                method: 'DELETE',
+                rel: 'delete_game'
+            },
+        ];
         var game = DB.games.find(g => g.id == id); //findOne
         if(game != undefined){
             var {title,year,price} = req.body;
